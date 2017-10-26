c357db1f63a2d292cca1110c909d36782ec3e0af
shashank biswas <shashankbiswasraj@gmail.com>
2017-10-26 23:40:16 +0530

    First commit

---
+++ Assets/Scripts/CoinCollection.js
@@ -0,0 +1,9 @@
+﻿function OnTriggerEnter(info : Collider){
+	var coinAudio: AudioSource=GetComponent.<AudioSource>();
+	coinAudio.Play();
+	CoinSystem.coincount++;
+	transform.position=Vector3(0,-1000,0);
+	yield WaitForSeconds(1.5);
+	Destroy(gameObject);
+
+}
---
+++ Assets/Scripts/Quests/Quest001End.js
@@ -0,0 +1,48 @@
+﻿//import UnityEngine.UI; // We need this to use the component in the TextBox
+
+var TextBoxOnCheck : int = 0; // Checks if TextBox is on the screen
+var MessageBox : GameObject; // Defines the message box as an object
+var TextBox : GameObject; // Defines the text box as an object
+var TextMessage : String; // The text we can change
+var QuestBox : GameObject; // Defines the quest box
+var QuestText : GameObject; // Defines the text in the Quest box
+var QuestName : String; // the quest name we can change
+//var bag: GameObject;
+function OnMouseDown () {
+			if (TextBoxOnCheck == 0) {
+				//bag.SetActive(true);
+				TextBoxOnCheck = 1; //Sets the variable to turn the box off
+				MessageBox.SetActive(true); //Re-enables the box
+				//TextBox.GetComponent.<Text>().text = "Villager: Hello baba, some thieves have stolen my gold, could you go retrieve them from the other side of the river?";
+				//TextBox.GetComponent.<Text>().text = TextMessage; //changes the message
+				QuestName = "Active Quest: None"; // change the quest name
+				QuestText.GetComponent.<Text>().text = QuestName; // input the quest name
+			}
+				
+			else {
+				TextBoxOnCheck = 0;
+				MessageBox.SetActive(false);
+				//TextBox.SetActive(false);
+				//TextMessage = "Villager: Get going then!";
+			}
+}
+
+// This bit enables us to use Enter or Escape button to cycle through text boxes
+
+function Update () {
+	if (Input.GetButtonDown("Submit")) {
+		if (TextBoxOnCheck == 1) {  //This needs to be set so as you need to be in a TextBox to do it
+				MessageBox.SetActive(false);
+				TextBoxOnCheck = 0;
+				//TextMessage = "Villager: Get going then!";
+			}
+		}
+		
+	if (Input.GetButtonDown("Cancel")) {
+		if (TextBoxOnCheck == 1) {
+				MessageBox.SetActive(false);
+				TextBoxOnCheck = 0;
+				//TextMessage = "Villager: Get going then!";
+			}
+		}
+}
